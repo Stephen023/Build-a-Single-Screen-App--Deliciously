8be280623de887bc869032c14a0fb547cc617ad9
Stephen023 <lovasis23@gmail.com>
2017-05-23 23:24:54 +0200

    Add files via upload

---
+++ Deliciously/app/src/main/java/com/example/android/deliciously/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.deliciously;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView lineTextView = (TextView) findViewById(R.id.textView_c);
+        lineTextView.setText(message);
+    }
+}
